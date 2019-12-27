72e2af3a271f6a6935c67edad62257282cba56ca
Jonas Cesconetto <jonasceesconetto@edu.univali.br>
2019-12-27 14:23:37 -0300

    Aula de modificacão do conteúdo das variáveis

---
+++ KotlinUdemyDemos/src/ModifyVariable.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    // A ideia desse video é demonstrar qeu é possível realizar alterações nas variáveis
+    var count = 1
+    println("Count: $count")
+
+    count += 1
+    println("Count: $count")
+
+    count += 5
+    println("Count: $count")
+
+    count -= 1
+    println("Count: $count")
+}
